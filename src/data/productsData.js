fd506a3e630dc23195449bbdbf431cb878bfe549
abhishek781 <20abhishek16@gmail.com>
2024-01-04 19:18:15 +0530

    first commit

---
+++ src/data/productsData.js
@@ -0,0 +1,68 @@
+const productsData = [
+    {
+        id: 1,
+        img: "/images/prod1.png",
+        rating: "★★★★★",
+        title: "Shirt",
+        price: 100,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        img: "/images/prod2.png",
+        rating: "★★★★",
+        title: "Shirt",
+        price: 100,
+        quantity: 1,
+    },
+    {
+        id: 3,
+        img: "/images/prod3.png",
+        rating: "★★★★★",
+        title: "Shirt",
+        price: 100,
+        quantity: 1,
+    },
+    {
+        id: 4,
+        img: "/images/prod4.png",
+        rating: "★★★★★",
+        title: "Shirt",
+        price: 100,
+        quantity: 1,
+    },
+    {
+        id: 5,
+        img: "/images/prod5.png",
+        rating: "★★★★",
+        title: "Shirt",
+        price: 100,
+        quantity: 1,
+    },
+    {
+        id: 6,
+        img: "/images/prod6.png",
+        rating: "★★★★",
+        title: "Shirt",
+        price: 100,
+        quantity: 1,
+    },
+    {
+        id: 7,
+        img: "/images/prod7.png",
+        rating: "★★★★★",
+        title: "Shirt",
+        price: 100,
+        quantity: 1,
+    },
+    {
+        id: 8,
+        img: "/images/prod8.png",
+        rating: "★★★★",
+        title: "Shirt",
+        price: 100,
+        quantity: 1,
+    }
+];
+
+export default productsData;
